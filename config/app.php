b6829bddb7b417e8e8d6a23dbd41799af327a96a
Vishalgoudar07 <Vishalgoudar143@gmail.com>
2026-05-26 15:46:39 +0530

    updated

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$basePath = getenv('APP_BASE_PATH') ?: '/php_invitation_system';
+$basePath = getenv('APP_BASE_PATH') ?: '';
 $localUrl = getenv('APP_LOCAL_URL') ?: 'http://localhost' . $basePath;
 $androidEmulatorUrl = getenv('APP_ANDROID_EMULATOR_URL') ?: 'http://10.0.2.2' . $basePath;
 $productionUrl = getenv('APP_PRODUCTION_URL') ?: 'https://drivault.com' . $basePath;
